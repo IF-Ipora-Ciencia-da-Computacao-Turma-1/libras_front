31dbacdea8f166043db615ee84542776d369506b
Mateus Barros Macedo <59882435+mateusbmm@users.noreply.github.com>
2023-05-14 23:05:28 -0300

    listar requisição
    pagina dois lista as requisições feitas em forma de lista

--- src/components/pagina2.js
+++ src/components/pagina2.js
@@ -2,39 +2,56 @@ import { useState } from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
 import axios from "axios";
   
+import Lista from "./lista";
+
 export default function App({navigator}) {
-    const [advice, setAdvice] = useState("");
-  
+
+    const [advice, setAdvice] = useState({});
+    let lista = ""
     
+   
     const getAdvice = () => {
-        console.log("PAssei aqui")
-        //axios.defaults.headers.common={}
-        axios.get("https://viacep.com.br/ws/01001000/json/")
+        //console.log("PAssei aqui")
+
+        
+        axios.get("https://viacep.com.br/ws/GO/Goiania/jas/json")
         .then((response) => {
-            console.log(response)
+            //console.log(response.data)
+            setAdvice(response.data)
+            console.log(advice)
         });
-      
+        //axios.defaults.headers.common={}
+        //console.log(lista)
+        //console.log("lista")
+
     };
   
     return (
         <View style={styles.container}>
-            <Text style={styles.advice}>{advice}</Text>
+            <Text style={styles.advice}>{advice.cep}</Text>
+            <Text style={styles.advice}>{advice.logradouro}</Text>
+           
             <Button title="Get Advice" 
                 onPress={getAdvice} color="green" />
+            <Lista style={styles.lista} props = {advice}></Lista>
         </View>
     );
 }
   
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
+    /*container: {
+       // backgroundColor: "#fff",
+       // width: '100vw',
+       // height: '100vh',
+       // alignItems: "center",
+       // justifyContent: "center",
     },
     advice: {
-        fontSize: 20,
-        fontWeight: "bold",
-        marginHorizontal: 20,
+        //fontSize: 20,
+        //fontWeight: "bold",
+        //marginHorizontal: 20,
     },
+    lista:{
+       // width:'100vw'
+    }*/
 });
--- src/css/css.js
+++ src/css/css.js
@@ -2,18 +2,18 @@ import { StyleSheet, Text, View} from 'react-native';
 
 
 const css = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+   /* container: {
+      //flex: 1,
+      ///backgroundColor: '#fff',
+      //alignItems: 'center',
+      //justifyContent: 'center',
     },
     container2: {
-        flex: 1,
-        backgroundColor: '#FF0000',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
+       // flex: 1,
+       // backgroundColor: '#FF0000',
+       // alignItems: 'center',
+        //justifyContent: 'center',
+      },*/
   });
 
   export {css};
