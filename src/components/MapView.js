397831d1289a35026d61c3ac512ceb449886cdae
Mateus Barros Macedo <59882435+mateusbmm@users.noreply.github.com>
2023-06-18 15:03:10 -0300

    mapCallOut

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, View } from "react-native";
-import MapView, { Marker } from "react-native-maps";
+import { SafeAreaView, StyleSheet, View,  } from "react-native";
+import MapView, { Marker, MapCallout } from "react-native-maps";
 
 const cidades = {
   cidade1: {
-    latitude: -16.2980797,
-    longitude: -50.1164483,
-    title: 'Cidade 1',
+    latitude: -16.6798608,
+    longitude: -49.3774167,
+    title: 'Goiânia',
     description: 'Descrição da Cidade 1',
   },
   cidade2: {
-    latitude: -17.2980797,
-    longitude: -50.1164483,
-    title: 'Cidade 2',
+    latitude: -16.4340564,
+    longitude: -51.147912,
+    title: 'Iporá',
     description: 'Descrição da Cidade 2',
   },
 }
@@ -48,11 +48,21 @@ export default function MapHomeComponent({navigation}){
                 description={cidade.description}
                 //onPress={() => navigation.navigate('VisualizarPin',{ cidade })}
                 icon={require('../imgs/Marker.png')}
-                
-              />
+                onCalloutPress={() => navigation.navigate('VisualizarPin',{ cidade })}
+               
+              >
+               
+               <MapCallout>
+
+               </MapCallout>
+
+              </Marker>
             );
           })}
         </MapView>
+
+         {/* Componente de informações que aparece ao clicar no marcador */}
+      
                 
             </View>
 
@@ -63,7 +73,16 @@ export default function MapHomeComponent({navigation}){
 }
 
 
-const styles = StyleSheet.create({
+
+const styles = StyleSheet.create({  
+    modalOverlay:{
+      width: 300,
+      height: 300,
+      backgroundColor: '#FF0000',
+
+    },
+
+
     SafeAreaView:{
         width: '100%',
         flex:   1,
